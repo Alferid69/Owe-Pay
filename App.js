e90fed09d7d20ff464180e82f3fc3d1a1b3bcf92
Alferid69 <nsr-1452-14@amu.edu.et>
2024-10-04 15:37:22 +0300

    gesture handler error

--- App.js
+++ App.js
@@ -5,38 +5,44 @@ import DebtDetails from "./DebtDetails";
 import EditDebt from "./EditDebt";
 import { StyleSheet, View } from "react-native";
 import Footer from "./Footer";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
+
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Stack.Navigator initialRouteName="DebtRegistration" screenOptions={{}}>
-          <Stack.Screen
-            name="DebtRegistration"
-            component={DebtRegistration}
-            options={{
-              title: "እዳ መመዝገቢያ",
-              headerShown: false,
-              headerTitleStyle: {
-                fontSize: 24, // Set your desired font size
-                fontWeight: "bold", // Set font weight
-                color: "#333", // Change text color
-                textAlign: "center", // Center the title
-              },
-              headerStyle: {
-                backgroundColor: "#f8f9fa", // Set background color of header
-              },
-            }}
-          />
-          <Stack.Screen name="DebtDetails" component={DebtDetails} />
-          <Stack.Screen name="EditDebt" component={EditDebt} />
-          {/* <Stack.Screen name="PayDebt" component={PayDebt} /> */}
-        </Stack.Navigator>
-        <Footer />
-      </View>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Stack.Navigator initialRouteName="DebtRegistration">
+            <Stack.Screen
+              name="DebtRegistration"
+              component={DebtRegistration}
+              options={{
+                title: "እዳ መመዝገቢያ",
+                headerShown: false,
+                headerTitleStyle: {
+                  fontSize: 24, // Set your desired font size
+                  fontWeight: "bold", // Set font weight
+                  color: "#333", // Change text color
+                  textAlign: "center", // Center the title
+                },
+                headerStyle: {
+                  backgroundColor: "#f8f9fa", // Set background color of header
+                },
+              }}
+            />
+            <Stack.Screen name="DebtDetails" component={DebtDetails} />
+            <Stack.Screen name="EditDebt" component={EditDebt} />
+          </Stack.Navigator>
+          <Footer />
+        </View>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
